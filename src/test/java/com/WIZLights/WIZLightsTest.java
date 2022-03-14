cf995ebf7fdbd40d94e5aa95227e2f5fca69074f
adnapPanda <robin_herlan@yahoo.com>
2022-03-14 13:19:30 +0100

    added return msg from bulb

--- src/test/java/com/WIZLights/WIZLightsTest.java
+++ src/test/java/com/WIZLights/WIZLightsTest.java
@@ -15,12 +15,14 @@ public class WIZLightsTest extends TestCase
     private InetAddress address;
 
     private byte[] buf;
+    private byte[] bufRecieved = new byte[1000];
 
     @Test
     public void WizLightTest() throws IOException {
         socket = new DatagramSocket();
-        address = InetAddress.getByName("192.168.2.18");
+        address = InetAddress.getByName("192.168.2.17");
         String msg = "{\"method\":\"setPilot\",\"params\":{\"state\":true,\"r\":255,\"g\":0,\"b\":230,\"w\":34}}";
+        //String msg = "{\"method\":\"getSystemConfig\"}";
         sendEcho(msg);
     }
 
@@ -29,5 +31,10 @@ public class WIZLightsTest extends TestCase
         DatagramPacket packet
                 = new DatagramPacket(buf, buf.length, address, 38899);
         socket.send(packet);
+        packet = new DatagramPacket(bufRecieved, bufRecieved.length);
+        socket.receive(packet);
+        String received = new String(
+                packet.getData(), 0, packet.getLength());
+        System.out.println(received);
     }
 }
